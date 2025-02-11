2997153c10bac15fcdf5d188e9e66c7f85c41af5
a-gss <36766244+a-gss@users.noreply.github.com>
2025-02-11 15:51:05 +0100

    Ajout regex
    foireux pour l'instant

--- gtfs.c
+++ gtfs.c
@@ -97,11 +97,11 @@ int main(int argc, char **argv)
     ///info("%s->%s [10:52]->[14:26] (3h34min)\n", orig, dest);
 
     // debug
-    printf("argc = %d\n", argc);
-    for (int i=0; i < argc; i++) printf("  argv[%d] = %s\n", i, argv[i]);
-    printf("date: %s\n", date);
-    printf("heure: %s\n", heure);
-    return 0;
+    //printf("argc = %d\n", argc);
+    //for (int i=0; i < argc; i++) printf("  argv[%d] = %s\n", i, argv[i]);
+    //printf("date: %s\n", date);
+    //printf("heure: %s\n", heure);
+    //return 0;
 
 
 
@@ -128,22 +128,22 @@ int main(int argc, char **argv)
     //  - files do not exist
     //  - files are older than the valid data (updated tout les 5 mois un truc comme ça)
     info("Downloading GTFS data... ");
-    system("curl --parallel -L --create-dirs "
-       "-s -o GTFS/calendar.txt 'https://data.laregion.fr/explore/dataset/reseau-lio/files/9bd5ef79fa139ccbce0511108584394b/download/' "
-       "-s -o GTFS/calendar_dates.txt 'https://data.laregion.fr/explore/dataset/reseau-lio/files/d39dc8f4edb7fa0cb7ed377a4b0f11f6/download/' "
-       "-s -o GTFS/routes.txt 'https://data.laregion.fr/explore/dataset/reseau-lio/files/92c45d9df99624d7e05e9ade35ba0ce8/download/' "
-       "-s -o GTFS/stop_times.txt 'https://data.laregion.fr/explore/dataset/reseau-lio/files/3cc9124c230b72e07df09e27c59eba88/download/' "
-       "-s -o GTFS/stops.txt 'https://data.laregion.fr/explore/dataset/reseau-lio/files/7068c8d492df76c5125fac081b5e09e9/download/' "
-       "-s -o GTFS/trips.txt 'https://data.laregion.fr/explore/dataset/reseau-lio/files/7831854a320cbf4ea5b6b327cd4581af/download/' "
-    );
+    //system("curl --parallel -L --create-dirs "
+    //   "-s -o GTFS/calendar.txt 'https://data.laregion.fr/explore/dataset/reseau-lio/files/9bd5ef79fa139ccbce0511108584394b/download/' "
+    //   "-s -o GTFS/calendar_dates.txt 'https://data.laregion.fr/explore/dataset/reseau-lio/files/d39dc8f4edb7fa0cb7ed377a4b0f11f6/download/' "
+    //   "-s -o GTFS/routes.txt 'https://data.laregion.fr/explore/dataset/reseau-lio/files/92c45d9df99624d7e05e9ade35ba0ce8/download/' "
+    //   "-s -o GTFS/stop_times.txt 'https://data.laregion.fr/explore/dataset/reseau-lio/files/3cc9124c230b72e07df09e27c59eba88/download/' "
+    //   "-s -o GTFS/stops.txt 'https://data.laregion.fr/explore/dataset/reseau-lio/files/7068c8d492df76c5125fac081b5e09e9/download/' "
+    //   "-s -o GTFS/trips.txt 'https://data.laregion.fr/explore/dataset/reseau-lio/files/7831854a320cbf4ea5b6b327cd4581af/download/' "
+    //);
     puts("OK");
 
     char *gtfs_data[GTFS_FILE_NUMBER];
     for (int i=0; i<GTFS_FILE_NUMBER; ++i) {
         info("Mapping '%s' into memory... ", gtfs_filenames[i]);
 
-        // Open the file in read-only
-        int fd = open(gtfs_filenames[i], O_RDONLY);
+        // Open the file in read/write
+        int fd = open(gtfs_filenames[i], O_RDWR);
         if (fd == -1) {
             perror("Error opening file");
             return EXIT_FAILURE;
@@ -156,8 +156,8 @@ int main(int argc, char **argv)
             return EXIT_FAILURE;
         }
 
-        // Mapping into memory: read-only, shared to other processes
-        gtfs_data[i] = mmap(NULL, file_size, PROT_READ, MAP_SHARED, fd, 0);
+        // Mapping into memory: read/write, shared to other processes
+        gtfs_data[i] = mmap(NULL, file_size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
         if (gtfs_data == MAP_FAILED) {
             perror("Error mapping file");
             close(fd);
@@ -182,11 +182,56 @@ int main(int argc, char **argv)
 
 
     // 1. Find the ID of the stations
-    printf("calendar dates:\n");
-    int i;
-    while(gtfs_data[calendar_dates][i] != '\0') {
-        putchar(gtfs_data[calendar_dates][i++]); // Access the data like an array
+    info("Searching for '%s' ID in stops.txt... ", orig);
+    char stop_id_pattern[128];
+    char stop_id[9] = "";
+    //snprintf(stop_id_pattern, sizeof(stop_id_pattern), "^([^,]*).*%s", orig);
+
+    regex_t regex;
+    if (regcomp(&regex, "31S16535", REG_EXTENDED) != 0) {
+        error("Impossible de compiler le regex.");
+        return EXIT_FAILURE;
     }
+
+    regmatch_t matches[2]; // Capture group for stop_id
+    char *save_ptr = NULL;
+    char *line = strtok_r(gtfs_data[stops], "\n", &save_ptr);
+    while (line) {
+        if (regexec(&regex, line, 2, matches, 0) == 0) {
+            int len = matches[1].rm_eo - matches[1].rm_so;
+            strncpy(stop_id, line + matches[1].rm_so, len);
+            stop_id[len] = '\0'; // Ensure null termination
+            printf("FOUND (%s)\n", stop_id);
+            break; // Stop after the first match
+        }
+        line = strtok_r(NULL, "\n", &save_ptr);
+    }
+
+
+
+    if (strcmp(stop_id, "31S16535") != 0)
+        error("pas le bon ID llolloll pont matabiau i guess\n");
+
+
+
+
+    regfree(&regex);
+    return 0;
+
+
+    int i;
+    //printf("%s", gtfs_data[stops]);
+    //while(1) {
+    //    if (gtfs_data[stops][i] == '\0') {
+    //        printf("\\0\n");
+    //        break;
+    //    } else if (gtfs_data[stops][i] == '\n') {
+    //        printf("\\n");
+    //    }
+    //    putchar(gtfs_data[stops][i]); // Access the data like an array
+    //    i++;
+    //}
+    //info("%s ID: %d\n", orig, 3);
 
 
 
