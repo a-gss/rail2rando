eef58f0789ce4731378e87f13a30d2e954a8a752
a-gss <36766244+a-gss@users.noreply.github.com>
2025-02-11 14:12:35 +0100

    Ajout des options cli

--- gtfs.c
+++ gtfs.c
@@ -1,45 +1,109 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <time.h>
+
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
+
 #include <regex.h>
 
 #define info(...)     do { printf("[INFO] "    __VA_ARGS__); fflush(stdout); } while (0)
 #define warning(...)  do { printf("[WARNING] " __VA_ARGS__); fflush(stdout); } while (0)
 #define error(...)    do { printf("[ERROR] "   __VA_ARGS__); fflush(stdout); } while (0)
 
-int main(int argc, char* argv[])
+void help(char **argv) {
+    printf("Usage : %s [options...] <origine>\n\n"
+
+            "Arguments :\n"
+            " <origine>                 Nom de la gare de départ (insensible à la casse)\n\n"
+
+            "Options :\n"
+            " -b, --bus                 Ajoute les bus dans le calcul d'itinéraire (le calcul sera plus long mais plus précis)\n"
+            " -d, --date <yyyy-mm-dd>   Précise la date de départ (défaut: aujourd'hui)\n"
+            "     --dest                Nom de la gare d'arrivée (insensible à la casse)\n"
+            " -h, --heure <hh:mm>       Précise l'heure de départ (défaut: 00:00)\n"
+            "     --help                Affiche ce texte\n"
+            " -l, --link                Affiche le lien LIO du trajet\n\n"
+
+            "Exemples :\n"
+            " %s Matabiau\n"
+            " %s -d 2025-09-25 --dest cahors saint-gaudens\n"
+            " %s -h 09:45 -b --dest cahors saint-agne\n"
+            " %s --bus --date 2025-04-16 --heure 09:45 --dest Matabiau --link Figeac\n",
+            argv[0],
+            argv[0],
+            argv[0],
+            argv[0],
+            argv[0]
+    );
+}
+
+int main(int argc, char **argv)
 {
+    // Initialize values
+    bool flag_bus = false;
+    bool flag_link_lio = false;
+    // Get current time
+    char date[11];             // yyyy-mm-dd\0
+    char heure[6] = "00:00";  // hh:mm\0
+    char orig[16];
+    char dest[16] = "";
+    // Init the date to today
+    strftime(date, sizeof(date), "%F", localtime(&(time_t){time(NULL)}));
+
+
     if (argc == 1) {
-        error("Vous devez spécifier une origine et une destination.\n");
-        printf("Usage : %s [options...] <origine> <destination>\n\n"
-
-                "Arguments :\n"
-                "<origine>                 Nom de la gare de départ (insensible à la casse)\n"
-                "<destination>             Nom de la gare d'arrivée (insensible à la casse)\n\n"
-
-                "Options :\n"
-                "-b, --bus                 Ajoute les bus dans le calcul d'itinéraire (le calcul sera plus long mais plus précis)\n"
-                "-d, --date <yyyy-mm-dd>   Précise la date de départ (défaut: aujourd'hui)\n"
-                "-h, --heure <hh:mm>       Précise l'heure de départ (défaut: 00:00)\n"
-                "    --help                Affiche ce texte\n"
-                "-l, --lio                 Affiche le lien LIO du trajet\n\n"
-
-                "Exemples:\n"
-                " %s Matabiau Figeac\n"
-                " %s -d 2025-09-25 cahors saint-gaudens\n"
-                " %s -h 09:45 -b cahors saint-agne\n"
-                " %s --bus --date 2025-04-16 --heure 09:45 Matabiau Figeac\n",
-                argv[0],
-                argv[0],
-                argv[0],
-                argv[0],
-                argv[0]
-        );
+        error("Vous devez spécifier une gare de départ <origine>.\n");
+        help(argv);
         return EXIT_FAILURE;
     }
+
+    // Get the argument
+    strcpy(orig, argv[argc - 1]);
+
+    // Get the options
+    for (int i=1; i < argc - 1; i++) {
+        if ((strcmp(argv[i], "--bus") == 0)  || (strcmp(argv[i], "-b") == 0)) {
+            flag_bus = true;
+
+        } else if ((strcmp(argv[i], "--date") == 0) || (strcmp(argv[i], "-d") == 0)) {
+            strncpy(date, argv[++i], 10);
+
+        } else if (strcmp(argv[i], "--dest") == 0) {
+            strcpy(dest, argv[++i]);
+
+        } else if ((strcmp(argv[i], "--heure") == 0) || (strcmp(argv[i], "-h") == 0)) {
+            strncpy(heure, argv[++i], 5);
+
+        } else if (strcmp(argv[i], "--help") == 0) {
+            help(argv);
+            return EXIT_SUCCESS;
+
+        } else if ((strcmp(argv[i], "--link") == 0) || (strcmp(argv[i], "-l") == 0)) {
+            flag_link_lio = true;
+
+        } else {
+            error("Invalid option.\n");
+            help(argv);
+            return EXIT_FAILURE;
+        }
+    }
+
+    info("Depart de %s à %s le %s\n", orig, heure, date);
+    ///info("%s->%s [10:52]->[14:26] (3h34min)\n", orig, dest);
+
+    // debug
+    printf("argc = %d\n", argc);
+    for (int i=0; i < argc; i++) printf("  argv[%d] = %s\n", i, argv[i]);
+    printf("date: %s\n", date);
+    printf("heure: %s\n", heure);
+    return 0;
+
+
 
     enum gtfs_index {
         calendar,
@@ -156,6 +220,6 @@ int main(int argc, char* argv[])
         puts("OK");
     }
 
-    printf("-----------------------------------------------------");
+    printf("-----------------------------------------------------\n");
     return EXIT_SUCCESS;
 }
